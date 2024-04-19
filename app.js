414264a0b525579ad606fc8285eeb26d1910ccf7
Vincent Bui <vincent.bui657@gmail.com>
2024-04-19 09:23:11 -0400

    Added controller

--- app.js
+++ app.js
@@ -1,33 +1,15 @@
-const express = require('express');
+import express from "express";
+import bodyParser from "body-parser";
+
+import usersRoutes from "./router.js";
+
 const app = express();
-const tasks = require('./routes/tasks');
-const connectDB = require('./db/connect');
-require('dotenv').config();
-const notFound = require('./middleware/not-found');
-const errorHandlerMiddleware = require('./middleware/error-handler');
+const PORT = 5000;
 
-// middleware
+app.use(bodyParser.json());
 
-app.use(express.static('./public'));
-app.use(express.json());
+app.use("/people", usersRoutes);
+app.get("/", (req, res) => res.send("Welcome to the Users API!"));
+app.all("*", (req, res) =>res.send("You've tried reaching a route that doesn't exist."));
 
-// routes
-
-app.use('/api/v1/tasks', tasks);
-
-app.use(notFound);
-app.use(errorHandlerMiddleware);
-const port = process.env.PORT || 5000;
-
-const start = async () => {
-  try {
-    await connectDB(process.env.MONGO_URI);
-    app.listen(port, () =>
-      console.log(`Server is listening on port ${port}...`)
-    );
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-start();
+app.listen(PORT, () =>console.log(`Server running on port: http://localhost:${PORT}`));
